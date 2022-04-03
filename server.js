4d0dba3ae448b578374155964e019eda30ed592e
jclos305 <jclos305@hotmail.com>
2022-04-03 18:22:06 -0400

    finished hw

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const app = require('./app');
-const initDb = require('./utils/db_connect');
-const checkDbConnection = require('./utils/checkDbConnection');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
 
-checkDbConnection();
+const helpers = require('./utils/helpers');
 
-//START SERVER
-const port = process.env.PORT || 4007;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'bigbluedog',
+  cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
